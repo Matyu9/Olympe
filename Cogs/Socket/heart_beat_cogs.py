e1335d0b39df97abb3fd68b1242e7b63cd78f822
Matyu9 <59774749+Matyu9@users.noreply.github.com>
2025-03-17 10:10:52 +0100

    - Passage à SQLAlchemy finis sur la partie socket
    Took 4 minutes

--- Cogs/Socket/heart_beat_cogs.py
+++ Cogs/Socket/heart_beat_cogs.py
@@ -1,14 +1,19 @@
 from flask_socketio import emit
 from time import time
 
+from Utils.Database.modules import Module
+
 def heart_beat_cogs(data, database):
-    fqdn_data = database.select('''SELECT * from cantina_administration.modules WHERE token = %s''', (data['token']), 1)
+    fqdn_data = database.query(Module).filter(Module.token == data["token"])
     server_time = round(time())
     saved_time = round((data['date']+server_time)/2)
 
-    if fqdn_data[3] == data["fqdn"] and fqdn_data[1] == data["token"]:
-        database.exec('''UPDATE cantina_administration.modules SET last_heartbeat = %s, status = 1
-                        WHERE token = %s''', (saved_time, data['token']))
+    if fqdn_data.fqdn == data["fqdn"] and fqdn_data.token == data["token"]:
+        database.query(Module).filter(Module.token == data["token"]).update({
+            "last_heartbeat": saved_time,
+            "status": True
+        })
+        database.commit()
 
         emit('response-heartbeat', {"receive-at": saved_time})
     else:
